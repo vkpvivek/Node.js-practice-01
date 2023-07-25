6f48c8efd2cca7a28cd516bec3e5ac4fda189bd2
vkpvivek <vkpvivek67@gmail.com>
2023-07-25 21:39:40 +0530

    arrow fn student object

--- first-app.js
+++ first-app.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+//console.log("hello world");
+var a=10;
+var b=20;
+
+//arrow fuction
+const mul=(a,b)=> a*b;
+console.log(mul(a,b));
+
+//object
+const student={
+    name:'Amar',
+    age:21,
+    course:'B.tech',
+    details(){
+        console.log('this is '+this.name+", age: "+this.age+" in course "+this.course);
+    }
+}
+
+student.details();
