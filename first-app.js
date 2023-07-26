b7ccad7e540d15ee63f59a7e36b718f25bb171c4
vkpvivek <vkpvivek67@gmail.com>
2023-07-26 09:21:42 +0530

    revise JS

--- first-app.js
+++ first-app.js
@@ -1,19 +1,53 @@
 //console.log("hello world");
-var a=10;
-var b=20;
+
 
 //arrow fuction
-const mul=(a,b)=> a*b;
-console.log(mul(a,b));
+        // var a=10;
+        // var b=20;
+        // const mul=(a,b)=> a*b;
+        //console.log(mul(a,b));
 
 //object
-const student={
-    name:'Amar',
-    age:21,
-    course:'B.tech',
-    details(){
-        console.log('this is '+this.name+", age: "+this.age+" in course "+this.course);
-    }
+        // const student={
+        //     name:'Amar',
+        //     age:21,
+        //     course:'B.tech',
+        //     details(){
+        //         console.log('this is '+this.name+", age: "+this.age+" in course "+this.course);
+        //     }
+        // }
+        //student.details();
+
+//object destructuring
+        // const {name, age, course}= student;
+        // console.log(name,age,course);
+
+//
+        // const arr=['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
+        // const newArr=arr.map((item)=>{
+        //     return item ===' ' ? 'empty string': item;
+        // })
+        // console.log(newArr);
+
+
+//Async & Await //and  Promises
+function  Delay(message,time){
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            console.log(message);
+            resolve();
+        },time);
+    });
 }
 
-student.details();
+async function print(){
+    console.log('a');
+    console.log('b');
+    await Delay('c',3000);
+    await Delay('d',0);
+    console.log('e');
+}
+
+print();
+
+ 
