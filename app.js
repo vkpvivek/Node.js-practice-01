90772c1e511c37eaf28b2ce5e76e61f5507bb12e
vkpvivek <vkpvivek67@gmail.com>
2023-07-27 23:10:08 +0530

    Custom Response based on Routes

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const http=require('http');
 
 const server=http.createServer((req,res)=>{
+    const url=req.url;
     // Set the response header
     res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-    // Send the response to the client
-    res.end('My name is Vivek Pal');
+    if(url==='/'){
+        res.end('Vivek Pal');
+
+    }else if(url==='/home'){
+        res.end('Welcome home');
+
+    }else if(url==='/about'){
+        res.end('Welcome to About Us page');
+
+    }else if(url==='/node'){
+        res.end(' Welcome to my Node Js project');
+        
+    }else{
+        res.end('My name is Vivek Pal');
+    }
 });
+
 
 const port=4000;
 server.listen(port,()=>{
