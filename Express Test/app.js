e33c89d821cd1bb7b506f142817a5a180ec9f389
vkpvivek <vkpvivek67@gmail.com>
2023-08-01 16:24:18 +0530

    commit body parser and routing

--- Express Test/app.js
+++ Express Test/app.js
@@ -1,16 +1,31 @@
 //const http=require('http');
 const express =require('express');
+const parseBody=require('body-parser');
 
 const app=express();
 
-app.use((req,res,next)=>{
-    console.log("i am in middleware");
+//for parsing the body
+app.use(parseBody.urlencoded({extended:false}));
+
+app.use('/',(req,res,next)=>{
+    console.log("this always run");
     next();
 });
 
-app.use((req,res,next)=>{
-    console.log("i am in another middleware");
-    res.send('<h1> hello to node js </h1>');
+app.use("/add-product",(req,res,next)=>{
+    console.log("i am in add-product middleware");
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="describe"><button type="submit">Add Product</button></form>');
+});
+
+app.use("/product",(req,res,next)=>{
+    console.log(req.body);
+    console.log("in product-page");
+    //res.send("<h1>product page</h1>");
+    res.redirect('/');
+});
+
+app.use('/',(req,res,next)=>{
+    res.send("<h1>Final page</h1>");
 });
 
 app.listen(4000);
